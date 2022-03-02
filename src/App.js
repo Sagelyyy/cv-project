06139c68877d810ef4b7660414b56447beb4d54e
Chris <orj.tsuk@gmail.com>
2022-03-02 08:05:33 -0500

    Setting the framework for the ui update.

--- src/App.js
+++ src/App.js
@@ -46,55 +46,7 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <div className='app--forms'>
-          <Generic
-            //General info
-            title='General Info'
-            onSubmit={this.handleSubmit}
-            onChange={this.handleInput}
-            handlePhone={this.handlePhone}
-            validPhone={this.state.validPhone}
-            inputs={['text', 'text', 'text', 'tel']}
-            placeholder={['First Name', 'Last Name', 'email@address.com', 'Phone',]}
-            names={['firstName', 'lastName', 'email', 'phone']}
-          />
-          <Generic
-            //Education
-            title='Education'
-            onSubmit={this.handleSubmit}
-            onChange={this.handleInput}
-            inputs={['text', 'text', 'date']}
-            placeholder={['Schoo Name', 'Degree', ' ']}
-            names={['school', 'studyTitle', 'studyDate']}
-            dateTitle={[null,null,'Year Graduated']}
-          />
-          <Generic
-            //Practical Experience
-            title='Experience'
-            onSubmit={this.handleSubmit}
-            onChange={this.handleInput}
-            inputs={['text', 'text', 'textarea', 'date', 'date']}
-            placeholder={['Company', 'Title', 'Tasks', 'Start Date', 'End Date']}
-            names={['company', 'position', 'jobTasks', 'jobStart', 'jobEnd']}
-            dateTitle={[null, null, null, 'Start Date','End Date']}
-          />
-
-        </div>
-        <Composed
-          // Anyway to make this more clean??
-          firstName={this.state.firstName}
-          lastName={this.state.lastName}
-          email={this.state.email}
-          phone={this.state.phone}
-          school={this.state.school}
-          studyTitle={this.state.studyTitle}
-          studyDate={this.state.studyDate}
-          company={this.state.company}
-          position={this.state.position}
-          jobTasks={this.state.jobTasks}
-          jobStart={this.state.jobStart}
-          jobEnd={this.state.jobEnd}
-        />
+ 
       </div>
     )
   }
---
+++ src/components/Personal.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Personal extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+
+            <div className="personal--container">
+
+            </div>
+        )
+    }
+}
